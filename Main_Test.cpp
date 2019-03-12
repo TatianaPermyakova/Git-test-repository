116b0002b24dcaf35c432773402bed32fadacb84
Tatiana <permyakova_tatiana_13@mail.ru>
2019-03-13 00:53:45 +0500

    Оформление консольного вывода
    Main_Test.cpp: дополнение исходного кода вводным сообщением, выводящимся в консоль приложения

--- Main_Test.cpp
+++ Main_Test.cpp
@@ -7,14 +7,14 @@
   typedef char _TCHAR;
   #define _tmain main
 #endif
-
+#include <iostream>
 #include <stdio.h>
 #include <windows.h>
 #include "Header.h"
-
+using namespace std;
 
 int _tmain()
-{
+{   cout << "This application created by..." << endl;
 	StudentName();
 	system ("PAUSE");
 	StudentGroup();

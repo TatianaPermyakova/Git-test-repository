6d8762eaf56216f9cc503a8456d3d60e2c1a8ecc
Tatiana <permyakova_tatiana_13@mail.ru>
2019-03-12 17:59:15 +0500

    Начальное состояние группы проектов Git test

---
+++ Main_Test.cpp
@@ -0,0 +1,21 @@
+#pragma hdrstop
+#pragma argsused
+
+#ifdef _WIN32
+#include <tchar.h>
+#else
+  typedef char _TCHAR;
+  #define _tmain main
+#endif
+
+#include <stdio.h>
+#include <windows.h>
+#include "Header.h"
+
+
+int _tmain()
+{
+	StudentName();
+	system ("PAUSE");
+	return 0;
+}
